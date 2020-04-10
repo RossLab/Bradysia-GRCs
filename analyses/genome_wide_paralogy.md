0e64087b23f7dfe9b232e1d4c1cd2a7136b7e4a5
Kamil S Jaroň <376090@mail.muni.cz>
2020-04-10 12:20:12 +0200

    arguemtns to to script

--- analyses/genome_wide_paralogy.md
+++ analyses/genome_wide_paralogy.md
@@ -14,7 +14,8 @@ gffread data/genome/annotation.gff3 -g data/genome/genome.fasta -w data/genome/t
 Now convert transcripts into gene using `data/genome/transcripts2genes.map` (always taking the longest transcript as the gene sequence)
 
 ```
-python3 scripts/genome_wide_paralogy/reduce_transcripts2genes.py
+python3 scripts/genome_wide_paralogy/reduce_transcripts2genes.py data/genome/transcripts2genes.map data/genome/transcripts.fasta | > data/genome/genes.fasta
+python3 scripts/genome_wide_paralogy/reduce_transcripts2genes.py data/genome/transcripts2genes.map data/genome/proteins.faa > data/genome/one_protein_per_gene.faa
 ```
 
 now we got a `data/genome/genes.fasta` file we can finally selfblast.
@@ -24,6 +25,8 @@ now we got a `data/genome/genes.fasta` file we can finally selfblast.
 ```
 GENES=data/genome/genes.fasta
 makeblastdb -in $GENES -dbtype nucl
+PROT=data/genome/one_protein_per_gene.faa
+makeblastdb -in $PROT -dbtype prot
 
 # blast all proteins vs all proteins
 qsub -o logs -e logs -cwd -N selfblast -V -pe smp64 16 -b yes "blastn -query $GENES -db $GENES -evalue 1e-10 -outfmt 6 -num_threads 16 | ~/generic_genomics/blast_filter.py 5 > data/genome_wide_paralogy/genes_all_vs_all.blast"
--- scripts/genome_wide_paralogy/reduce_transcripts2genes.py
+++ scripts/genome_wide_paralogy/reduce_transcripts2genes.py
@@ -2,31 +2,35 @@
 
 from collections import defaultdict
 from Bio import SeqIO
+import argparse
+from sys import stdout
+
+parser = argparse.ArgumentParser(description='Given gene2transcript map extract longest transcript per gene from an fa/faa file.')
+parser.add_argument('g2t_map', help='path to gene to transcript map (2 column tsv file)')
+parser.add_argument('transcriptome', help='fasta or faa file (headers must match transcripts in the map)')
+
+args = parser.parse_args()
 
 transcripts2genes = dict()
-gene2transcript_mapfilename = 'data/genome/transcripts2genes.map'
-with open(gene2transcript_mapfilename, 'r') as g2t_file:
+with open(args.g2t_map, 'r') as g2t_file:
     for line in g2t_file:
         gene, trainscript = line.rstrip('\n').split('\t')
         transcripts2genes[trainscript] = gene
 
 genes2transcript_seuqneces = defaultdict(list)
-transcripts_fasta_filename = 'data/genome/transcripts.fasta'
-ffile = SeqIO.parse(transcripts_fasta_filename, 'fasta')
+ffile = SeqIO.parse(args.transcriptome, 'fasta')
 for seq_record in ffile:
     gene = transcripts2genes[seq_record.name]
     genes2transcript_seuqneces[gene].append(seq_record)
 
 # read transcripts
-genes_fasta_filename = 'data/genome/genes.fasta'
-with open(genes_fasta_filename, 'w') as out_file:
-    for gene in genes2transcript_seuqneces.keys():
-        transcripts = genes2transcript_seuqneces[gene]
-        lengest = 0
-        for index, tr in enumerate(transcripts):
-            if len(transcripts[lengest].seq) > len(tr.seq):
-                lengest = index
-            # if lengest != 0:
-            #     print(lengest)
-        fasta_string = '>' + gene + '\n' + str(transcripts[lengest].upper().seq) + '\n'
-        out_file.write(fasta_string)
+for gene in genes2transcript_seuqneces.keys():
+    transcripts = genes2transcript_seuqneces[gene]
+    lengest = 0
+    for index, tr in enumerate(transcripts):
+        if len(transcripts[lengest].seq) > len(tr.seq):
+            lengest = index
+        # if lengest != 0:
+        #     print(lengest)
+    fasta_string = '>' + gene + '\n' + str(transcripts[lengest].upper().seq) + '\n'
+    stdout.write(fasta_string)
