d01e3d64339ca1f4783b1836b67004a1885dd913
KamilSJaron <kamiljaron@gmail.com>
2021-10-06 14:19:53 +0100

    fixing the reciprocal blast (should be a lot less memory intense now)

--- scripts/genome_wide_paralogy/reciprocal_blast.py
+++ scripts/genome_wide_paralogy/reciprocal_blast.py
@@ -3,6 +3,7 @@
 from collections import defaultdict
 import argparse
 from sys import stdout
+from sys import stderr
 
 parser = argparse.ArgumentParser(description='Reciprocal BLAST: from blast std output 6 generates two tables of orthologs as two with reciprocal hits fulfilling specified criteria')
 parser.add_argument('blastfile', help='blast file formated as -outfmt "6 std qlen slen"')
@@ -12,8 +13,8 @@ parser.add_argument('-gene_coverage', type = float, help='minimal fraction of ge
 
 args = parser.parse_args()
 # args.blastfile = 'data/genome_wide_paralogy/proteins_all_vs_all.blast'
-# args.output_pattern = 'data/genome_wide_paralogy/aa_orthology_40'
-# args.s = 80
+# args.output_pattern = 'data/genome_wide_paralogy/aa_homology_test_85'
+# args.s = 85
 # args.gene_coverage = 0.6
 
 class blast_std6_qlen_slen:
@@ -44,14 +45,14 @@ with open(args.blastfile, 'r') as blastf:
         aln = blast_std6_qlen_slen(entry)
         if aln.subject != aln.query:
             blast_input_dict[aln.query].append(aln)
+# blast_input_dict['jg28688'], aims at jg5502 and jg30732
 
-# curate alignments
-# build dictionary of relations (every quary points to the list of subjects it's aligned to)
-# query = 'jg27403'
+# curate alignments by
+#    merging all alignments into single per a pairt of subject and query
+#    building dictionary of alignments (q, s) -> aln
+# query = 'jg28688' - good testing one, has 2 hits jg5502 and jg30732
 blast_input = dict()
 for query in blast_input_dict.keys():
-    # ALL ADJUSTMENTS of the contition for gene <-> gene orthology
-    # SHOULD BE ADDED HERE
     aln_list = defaultdict(list)
     for aln in blast_input_dict[query]:
         aln_list[aln.subject].append(aln)
@@ -71,23 +72,42 @@ for query in blast_input_dict.keys():
                 final_q2s_aln.len = final_q2s_aln.len + other_aln.len
             final_q2s_aln.qstart = start
             final_q2s_aln.qend = end
-        blast_input[tuple(sorted((aln.query, aln.subject)))] = (final_q2s_aln)
+        blast_input[(aln.query, subject)] = final_q2s_aln
+
+# all three following should be in the disctionary
+# blast_input[('jg28688', 'jg5502')]
+# blast_input[('jg5502', 'jg28688')]
+# and all the combinaiton with jg30732
 
 del blast_input_dict
 
+# now build a unidirection graph of relations given
+#     similarity condition
+#     align / gene length condition
 query2subject = defaultdict(list)
 for aln in blast_input.values():
-    # ALL ADJUSTMENTS of the contition for gene <-> gene orthology
-    # SHOULD BE ADDED HERE
+    ################################################################
+    # ALL ADJUSTMENTS of the contition for gene <-> gene orthology #
+    # SHOULD BE ADDED HERE                                         #
+    ################################################################
     if aln.pident > args.s and (aln.len / aln.qlen) > args.gene_coverage:
         query2subject[aln.query].append(aln.subject)
 
-# query2subject['jg21908']
+# query2subject['jg28688']
+# query2subject['jg5502']
+# query2subject['jg30732']
 
+# setting up variables to
+
+# keep track of nodes with outther braches explored
 gene2processed = defaultdict(bool)
-list_of_genes = list(query2subject.keys())
+# a list for genes that will be processed per orthogroup
 queue_of_genes = list()
-og = 0
+# OG number
+og = 1
+# again, tested on the popular trio of orthologs
+# stderr.write('You forgot to comment follwing line\n')
+# queue_of_genes = ['jg28688']
 
 og_file = open(args.output_pattern + "_OGs.tsv", 'w')
 og_pairs_file = open(args.output_pattern + "_OG_pairs.tsv", 'w')
@@ -95,47 +115,54 @@ og_pairs_file.write("\t".join(['og', 'gene1', 'gene2', 'identity', 'aln_len', 'q
 
 # print('everything is ready')
 # go through all the genes
-for top_bot_gene_iterator in list_of_genes:
+for top_bot_gene_iterator in list(query2subject.keys()):
     # but skip all those that were processed already
     if gene2processed[top_bot_gene_iterator]:
         continue
-    # unprocessed gene means a new orthogroup
-    og += 1
-    # and the queue of genes to be processed in this orthogroup starts with that one gene
-    # queue_of_genes = ['jg12063']
+
+    # set up the queue of genes to be processed in this potential orthogroup which starts with that one gene
+    # queue_of_genes = ['jg28688']
     queue_of_genes = [top_bot_gene_iterator]
     # here I will store all the info about the orthogroup
-    gene2og = defaultdict(str)
-    og2gene = defaultdict(list)
+    og2gene = list()
     og_pairs = set()
     # now go through queue_of_genes list till it's empty
     while not queue_of_genes == []:
+        # stderr.write(gene + '\n')
         # pop will always pop(0) the 1st element to gene
         gene = queue_of_genes.pop(0)
+        og2gene.append(gene)
+        gene2processed[gene] = True
         # and here I want to process all it's orthologs
         for ortholog in query2subject[gene]:
             # proceed only if the blast is reciprocal
             if not gene in query2subject[ortholog]:
+                # stderr.write(ortholog + ' is not reciprocal\n')
                 continue
-            # this will stop infinite loops
+            # do not explore already explored branches
             if gene2processed[ortholog]:
+                # stderr.write(ortholog + ' is already processed\n')
                 continue
-            # this gene needs to be process within this orthogroup
-            queue_of_genes.append(ortholog)
-            # but it won't be considered next time
-            gene2processed[ortholog] = True
+            # this gene needs to be process within this orthogroup (unless it's already queued for processing)
+            if not ortholog in queue_of_genes:
+                # stderr.write('Adding ' + ortholog + ' to the queue\n')
+                queue_of_genes.append(ortholog)
+
             og_pairs.add(tuple(sorted((gene, ortholog))))
-            gene2og[ortholog] = og
-            gene2og[gene] = og
-            og2gene[og].append(ortholog)
-            og2gene[og].append(gene)
 
-    og_name = 'og_' + str(og)
-    og_file.write("\t".join([og_name] + og2gene[og]) + '\n')
+    # record the group only if it contains at least two genes
+    if len(og2gene) > 1:
+        og_name = 'og_' + str(og)
+        og_file.write("\t".join([og_name] + og2gene) + '\n')
 
-    for og_pair in og_pairs:
-        for aln in blast_input[og_pair]:
-            og_pairs_file.write("\t".join([og_hast2og_name[gene2og[aln.query]], aln.query, aln.subject, str(aln.pident), str(aln.len),  str(aln.qlen), str(aln.slen)])  + '\n')
+        for g1, g2 in og_pairs:
+            aln = blast_input[g1, g2]
+            aln2 = blast_input[g2, g1]
+            aln.pident = round(((aln.pident * aln.len) + (aln2.pident * aln2.len)) / (aln.len + aln2.len), 3)
+            aln.len = (round(aln.len + aln2.len) / 2)
+            og_pairs_file.write("\t".join([og_name, aln.query, aln.subject, str(aln.pident), str(aln.len),  str(aln.qlen), str(aln.slen)])  + '\n')
+
+        og += 1
 
 og_file.close()
 og_pairs_file.close()
